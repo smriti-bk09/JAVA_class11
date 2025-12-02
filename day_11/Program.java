9b417f871358a7c2cb806abbc01cb0feb32fab71
smriti-09 <smritipompom09@gmail.com>
2025-12-02 02:23:16 -0800

    user

--- day_11/Program.java
+++ day_11/Program.java
@@ -1,3 +1,21 @@
 package day_11;
-import java.util.Scanner
+import java.util.Scanner;
+public class Program{
+    public static void main(String[]args){
+
+        Scanner input = new Scanner(System.in); 
+        System.out.println("Enter the length ");
+        double length =input.nextDouble();
+
+        System.out.println("Enter the breadth ");
+        double breadth =input.nextDouble();
+
+ Rectangle rectangle =new Rectangle(length,breadth);
+ rectangle.areaOfRectangle();
+ rectangle.perimeterOfRectangle();
+
+    }
+}
+
+
 
